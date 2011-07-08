bc37daf1c185b18f6fb5eea297c6830de295ad69
Jackson Lin <think.jackson@gmail.com>
2011-07-07 22:11:21 -0400

    Set up Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Bp::Application.routes.draw do
+
+  devise_for :users
+  
+  resources :home, :only => [:index]
+  
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,8 +54,8 @@ Bp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-
+   root :to => "home#index"
+  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
