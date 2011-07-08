f172fed9a1c3f0fc1c26d534dea9f456cebb6c2d
Jackson Lin <think.jackson@gmail.com>
2011-07-08 00:00:27 -0400

    layout and homelink

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Bp::Application.routes.draw do
-
-  devise_for :users
   
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  root :to => "home#index"
+   
   resources :home, :only => [:index]
-  
+   
+  devise_for :users
+    
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -51,10 +55,6 @@ Bp::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-   root :to => "home#index"
   
   # See how all your routes lay out with "rake routes"
 
